5ccd00b411dde0503d3e3fda66c3defbab84a1f0
Manish Sharma <76739229+Mani1183@users.noreply.github.com>
2026-01-08 22:55:09 +0530

    Comment out
    Comment out the prime number checking code in task.js

--- homework/js/task.js
+++ homework/js/task.js
@@ -220,19 +220,19 @@
 
 // Task 10: Check Prime Number
 
-let num = 5;
-let isPrime = true;
-for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-        isPrime = false;
-        break;
-    }
-}
-if (isPrime && num > 1) {
-    console.log("Prime Number");
-} else {
-    console.log("Not a Prime Number");
-}  
+// let num = 5;
+// let isPrime = true;
+// for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//         isPrime = false;
+//         break;
+//     }
+// }
+// if (isPrime && num > 1) {
+//     console.log("Prime Number");
+// } else {
+//     console.log("Not a Prime Number");
+// }  
 
 
 
