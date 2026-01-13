7c1a82af82511d54ef9a97e0ebc01a7f5d4b58ee
Manish kumar <manish1182003@gmail.com>
2026-01-13 12:10:13 +0530

    todo CI

--- homework/js/todo.js
+++ homework/js/todo.js
@@ -1,13 +1,16 @@
+
+function claculateCompoundInterest(principal, rate, time) {
+  return principal.map(p => {
+    let amount = p;
+    for (let i =1; i<=time; i++) {
+      amount += amount * rate / 100;
+    }
+    return amount - p;
+  });
+}
+
 let principal = [100, 200, 300];
 let rate = 5;
-let time = 1;
-
-let compoundInterest = principal.map(p => {
-  let amount = p;
-  for (let i = 1; i <= time; i++) {
-    amount += amount * rate / 100;
-  }
-  return amount - p;
-});
-
-console.log(compoundInterest);
+let time = 2;
+let interest = claculateCompoundInterest(principal, rate, time);
+console.log(interest);
